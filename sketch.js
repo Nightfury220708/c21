076fbd400e824d21816a5b82553ff5a2a9120f5b
Nightfury220708 <75155760+Nightfury220708@users.noreply.github.com>
2020-12-06 11:55:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+isTouching(movingRect,fixedRect);
+  
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
